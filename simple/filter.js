174fe213ab46b5da60c685139a27e7f5cd25b404
Ivan Zykov <ivan.v.zykov@gmail.com>
2018-06-30 17:16:13 +0200

    Adds comments to filter.js

--- simple/filter.js
+++ simple/filter.js
@@ -2,12 +2,16 @@ function filter() {
 	//Read the chosen office
 	let office = document.getElementById('office').value;
 	xhttp = new XMLHttpRequest();
+	//When response of the request arrives
 	xhttp.onreadystatechange = function () {
+		//Responce OK?
 		if (this.readyState == 4 && this.status == 200) {
+			//work with responce
 			//Assign responce from php file to table body
 			document.getElementById('table').innerHTML = this.responseText;
 		}
 	}
+	//Try to execute the request
 	xhttp.open('POST', 'filterOffice.php', true);
 	xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded'); //!!! Required?
 	xhttp.send('office=' + office);
